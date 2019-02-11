765a7a0c1bd803d0116a39dc26899a35ce7c3ff5
anthonygilbertt <anthony.gilbert93@gmail.com>
2019-02-11 03:25:34 -0600

    Adds DELETE method

--- app.js
+++ app.js
@@ -4,51 +4,91 @@ const app = express();
 app.use(express.json());
 
 const courses = [
-    { id: 1, name: 'math'},
-    { id: 2, name: 'astronomy'},
-    { id: 3, name: 'computer science'}
+  { id: 1, name: 'math' },
+  { id: 2, name: 'astronomy' },
+  { id: 3, name: 'computer science' }
 ]
 
 app.get('/', (req, res) => {  //this takes 2 args( 1. path or URL, 2. callback function )
-    res.send('Hello World!!!');
-    res.end();
+  res.send('Hello World!!!');
+  res.end();
 });
 
 app.get('/api/courses', (req, res) => {
-    //Typically this is where we get a list of courses from the database
-    res.send(courses);
-    //in the future, we can redirect the user to a file called 'courses.js'
+  //Typically this is where we get a list of courses from the database
+  res.send(courses);
+  //in the future, we can redirect the user to a file called 'courses.js'
 });
 
 
 app.post('/api/courses', (req, res) => {
-    //Joi schema 
-    const schema = Joi.object().keys({
-        // id: Joi.number().integer().min(1).max(255).required(),
-        name: Joi.string().min(3).required()
-    }).with('id', 'name');//
-
-    const result = Joi.validate(req.body, schema);
-    //here we can write some validation logic
-    //you always want to write validation logic at the beginning of the route handler
-if (result.error) {    
-    res.status(400).send(result.error.details[0].message); //400  Bad request
+  const { error } = validateCourse(req.body); //result.error
+  if (error) {
+    res.status(400).send(error.details[0].message); //400 - Bad request
     return;
+  }
+
+  let course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+  //look up the course ID
+  //if the course ID does not exist, we return 404
+  let course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found'); //404 -> this means Object not found
+
+  //otherwise we need to validate the course - Joi schema 
+  //If Invalid, return 400 - Bad Request
+
+  // const result = validateCourse(req.body); //we can refactor and make this cleaner by using 
+  //the object destructure feature and modern javascript
+  const { error } = validateCourse(req.body); //result.error
+
+  if (error) {
+    res.status(400).send(error.details[0].message); //400  Bad request
+    return;
+  }
+
+  //Update the course
+  course.name = req.body.name;
+  //Return updated course
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    // id: Joi.number().integer().min(1).max(255).required(),
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(course, schema);
 }
 
-    let course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
+
+app.delete('/api/courses/:id', (req, res) => {
+  //look up the course
+  let course = courses.find(c => c.id === parseInt(req.params.id));
+  //if it does not exist -return 404
+  if (!course) res.status(404).send('The course with the given ID was not found'); //404 -> this means Object not found
+  // otherwise if it exists, delete it
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+
+
 });
 
 
 app.get('/api/courses/:id', (req, res) => {
-    let course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with the given ID was not found'); //404 -> this means Object not found
-    res.send(course);
+  let course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found'); //404 -> this means Object not found
+  res.send(course);
 });
 
 
